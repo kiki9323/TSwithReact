03a0e5858a86e645a70a13b3c291161198922be2
noodle <noodle>
2023-11-18 15:14:00 +0900

    feat: this 바인딩 대신 데코레이터 적용

--- docs/README.md
+++ docs/README.md
@@ -1,6 +1,50 @@
+### !, as~
+
 ```ts
 this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
 // `!` : null이 아님을 확실히 알고 있으므로 !를 추가한다.
-// `as ~` : 형변형
+// `as ~` : getElementById이 뭘 반환할지 모르기 때문에 형변형(casting)
+```
+
+---
+
+### 데코레이터로 바인딩 문제 해결하기
+
+```js
+// .bind(this) 혹은 화살표 함수로 바인딩문제를 해결할 수 있다.
+private configure() {
+  this.element.addEventListener('submit', this.submitHanlder.bind(this));
+}
+```
+
+```js
+// 데코레이터 방법도 있다.
+// autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+// 이벤드핸들러의 콜백함수를 정의한 곳에서 아래와 같이 사용
+@autobind;
+```
+
+### 튜플 정의하는 방법
+
+튜플 : 요소의 개수와 타입이 고정된 튜플 (TS)
+
+- 요구 사항: 정확히 세 가지 타입의 x개의 요소로 된 배열을 리턴하려고 한다.
+
+```ts
+private gatherUserInput(): [string, string, number] | void {
+  // ...
+}
 ```
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,24 @@
+// autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+// ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -10,8 +27,58 @@ class ProjectInput {
     // 즉시 렌더링을 하기 위해서 생성자 함수 내부에서 실행해본다.
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
-    console.log(this.templateElement, this.templateElement.content);
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description')! as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredpeople = this.peopleInputElement.value;
+
+    // 입력값이 비어있는지 검사
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredpeople.trim().length === 0
+    ) {
+      alert('유효하지 않는 입력값입니다. 다시 시도해주세용.');
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, parseInt(enteredpeople)];
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.peopleInputElement.value = '';
+  }
+
+  @autobind
+  private submitHanlder(event: Event) {
+    event.preventDefault(); //  기본 동작인 http 전송하는 양식 제출 방지
+    const userInput = this.gatherUserInput();
+
+    // ts에서는 튜플이지만, js에서는 그냥 배열일 뿐. 배열인지 검사
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      this.clearInputs();
+      console.log(title, desc, people);
+    }
+  }
+
+  /**
+   * Event Listener
+   */
+  private configure() {
+    this.element.addEventListener('submit', this.submitHanlder);
   }
 
   private attach() {
